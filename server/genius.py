971bf418f8c2d8ffbe8f9bf84e12a7eaa09b214c
kendall-sullivan <123098060+kendall-sullivan@users.noreply.github.com>
2025-04-25 10:39:42 -0500

    clean lyrics

--- server/genius.py
+++ server/genius.py
@@ -2,9 +2,11 @@
 # https://www.youtube.com/watch?v=CokKFmDnDtw
 # https://github.com/johnwmillr/LyricsGenius
 
+import re
+import string
 import lyricsgenius
 
-genius = lyricsgenius.Genius("79bSVlRX4YEmwomC2oIp_jiWPiGliEtArd2dsIlisD4NfHPPVuRdp-skYhQKmfgn")
+genius = lyricsgenius.Genius("79bSVlRX4YEmwomC2oIp_jiWPiGliEtArd2dsIlisD4NfHPPVuRdp-skYhQKmfgn", remove_section_headers=False)
 # artist = genius.search_artist("Taylor Swift", max_songs=1)
 # songs = artist.songs
 # for song in songs:
@@ -14,14 +16,29 @@ def coverArt(title, artist):
     song = genius.search_song(title, artist)
     return song.song_art_image_thumbnail_url
 
+def clean_lyrics(lyrics):
+    song_start = re.search(r'\[.*?\]', lyrics)
+    if song_start:
+        lyrics = lyrics[song_start.start():]
+
+    lyrics = re.sub(r'\[.*?\]', ' ', lyrics)
+    lyrics = lyrics.replace('\n', ' ')
+    lyrics = lyrics.replace("'", '')
+    lyrics = re.sub(r'''      # used https://stackoverflow.com/questions/34860982/replace-the-punctuation-with-whitespace
+               \W+
+               \s*
+               ''',
+               ' ',
+               lyrics,
+               flags=re.VERBOSE)
+    return lyrics
+
+
 def getLyrics(title, artist):
     song = genius.search_song(title, artist)
-    return song.lyrics
-
-def searchMulti(term):
-    results = genius.search(search_term=term, per_page=20, type_='song')
-    return results
-    
+    if song:
+        return clean_lyrics(song.lyrics)
+    return "Lyrics not found"
 
 # song = genius.search_song("firework", "Katy Perry")
 # print(song.lyrics)
